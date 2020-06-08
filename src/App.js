9221b59d8b5694d6bafa9a1e79aea0cbe7c06410
spantheslayer <supriyosarkar282@gmail.com>
2020-06-08 13:29:02 +0530

    Sorted out all the unncessary files

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-          Learn React
+    <div className="app">
+        <header className="div">
+            <h1 className="name">Spantheslayer Isnab</h1>
+        </header>
     </div>
   );
 }
