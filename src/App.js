f1d1530b496cc74e8f91be0be22af79568e0b93b
Supriyo <supriyosarkar282@gmail.com>
2020-06-11 10:38:16 +0530

    added a layout and some js to work the todo form . And also some css . The tutorial by code spot (to do app in react) .

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
 import React from 'react'
+import "./App.css"
 
-function IAmAH1() {
-  return <h1> Hello , This is testing</h1>
-}
-
-function App() {
-  return (
-    <IAmAH1 />
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      items:[],
+      currentItem:{
+        text:'',
+        key: ''
+      }
+    }
+    this.handleInput = this.handleInput.bind(this)
+    this.addItem = this.addItem.bind(this)
+  }
+  handleInput(e){
+    this.setState({
+      currentItem:{
+        text: e.target.value,
+        key: Date.now()
+      }
+    })
+  }
+  addItem(e){
+    e.preventDefault()
+  }
+  render() {
+    return (
+      <div className="App">
+          <header>
+            <form id="to-do-form" onSubmit={this.addItem}>
+              <input type="text" placeholder="Enter Text" value={this.state.currentItem.text}onChange={this.handleInput}/>
+              <button type="submit">Add</button>
+            </form>
+          </header>
+      </div>
+    )
+  }
 }
 
 export default App
