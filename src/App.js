564e023b5a09195b289aa9dc4892ffc789fadd55
Supriyo <supriyosarkar282@gmail.com>
2020-06-12 15:52:32 +0530

    Final code for the application in test mode

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import "./App.css"
 import ListItems from './ListItems'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
 
+library.add(faTrash)
 class App extends React.Component {
   constructor(props){
     super(props);
@@ -14,6 +17,8 @@ class App extends React.Component {
     }
     this.handleInput = this.handleInput.bind(this)
     this.addItem = this.addItem.bind(this)
+    this.deleteItem = this.deleteItem.bind(this)
+    this.setUpdate = this.setUpdate.bind(this)
   }
   handleInput(e){
     this.setState({
@@ -37,6 +42,23 @@ class App extends React.Component {
       })
     }
   }
+  deleteItem(key){
+    const filteredItems = this.state.items.filter(items => items.key!==key)
+    this.setState({
+      items:filteredItems
+    })
+  }
+  setUpdate(text, key) {
+    const items = this.state.items
+    items.map(item => {
+      if(item.key===key) {
+        item.text=text
+      }
+    } )
+    this.setState({
+      items: items
+    })
+  }
   render() {
     return (
       <div className="App">
@@ -46,7 +68,8 @@ class App extends React.Component {
               <button type="submit">Add</button>
             </form>
           </header>
-          <ListItems items = {this.state.items}></ListItems>
+          <ListItems items = {this.state.items} deleteItem = {this.deleteItem}
+          setUpdate = {this.setUpdate}></ListItems>
       </div>
     )
   }
--- src/ListItems.js
+++ src/ListItems.js
@@ -1,17 +1,36 @@
 import React from 'react'
 import './ListItems.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import FlipMove from 'react-flip-move'
 
 function ListItems(props) {
     const items = props.items
     const listItems = items.map(item => {
 
-        return <div className="list" key="itemm.key">
-            <p>{item.text}</p>
+        return <div className="list" key={item.key}>
+            <p>
+                <input type="text" 
+                id={item.key} 
+                value = {item.text}
+                onChange = {
+                    (e) => {
+                        props.setUpdate(e.target.value, item.key)
+                    }
+                }
+                />
+                <span>
+                    <FontAwesomeIcon className="faicons" icon='trash' onClick={ () => props.deleteItem(item.key)}></FontAwesomeIcon>
+                </span>
+            </p>
         </div>
 
     })
     return(
-        <div>{listItems}</div>
+        <div>
+            <FlipMove duration={200} easing="ease-in-out" >
+            {listItems}
+            </FlipMove>
+        </div>
     )
 }
 
